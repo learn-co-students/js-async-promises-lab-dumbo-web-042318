7a394eddc8f7c41d3e077bb529a93f02c1c43d02
jessebillard <jessebillard@mac.com>
2018-06-08 14:54:25 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,48 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question) {
+  const questionContainer = document.querySelector('.question-container')
+  questionContainer.innerHTML = question.questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise ((resolve, reject) => {
+    setTimeout(function() {
+      resolve(question)
+    }, time)
+  })
+}
+
+function removeQuestion() {
+  const questionContainer = document.querySelector('.question-container')
+  return new Promise ((resolve, reject) => {
+    resolve(questionContainer.innerHTML = '')
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  const button = document.querySelector('.true-false-list').querySelectorAll(".btn")
+  return button
+}
+
+function toggleTrueAndFalseButtons() {
+  const btnArray = trueAndFalseButtons()
+  btnArray[0].classList.toggle("hide")
+  btnArray[1].classList.toggle("hide")
+}
+
+function displayQuestionOnClick() {
+  toggleTrueAndFalseButtons()
+  askQuestionThenRemoveQuestion(1000).then(toggleTrueAndFalseButtons)
+}
+
+const askAway = document.querySelector('#ask')
+askAway.addEventListener('click', displayQuestionOnClick)
