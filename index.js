11fa5f2246132282dd0b0b5b0bf7fccfa6aa2bb6
travisdock <travisdock@gmail.com>
2018-06-11 12:20:43 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question) {
+  qBox = document.querySelector('.question-container');
+  qBox.innerHTML = question["questionText"];
+}
+
+function askQuestionThen(time){
+  question = questions[0];
+  appendQuestion(question);
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve(question)
+    }, time )
+  })
+}
+
+function removeQuestion(){
+  document.querySelector('.question-container').innerHTML = ''
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time)
+    .then(removeQuestion)
+    .catch(console.log)
+}
+
+function trueAndFalseButtons() {
+  let buttons =  document.querySelector('.true-false-list').querySelectorAll('.btn')
+  buttons.forEach(btn => btn.innerText = btn.innerText.trim().toUpperCase())
+  return buttons
+}
+
+function toggleTrueAndFalseButtons() {
+  let buttons = trueAndFalseButtons()
+  buttons.forEach(function(btn){
+    if (btn.classList.contains('hide')) {
+      btn.classList.remove('hide')
+    } else {
+      btn.classList.add('hide')
+    };
+  })
+};
+
+function displayQuestionOnClick() {
+  let button = document.querySelector('.waves-light, .btn')
+  button.addEventListener("click", console.log("clicked"))
+  askQuestionThenRemoveQuestion(10000)
+  toggleTrueAndFalseButtons()
+}
